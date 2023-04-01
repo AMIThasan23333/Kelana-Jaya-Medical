1a38a05a03e0bacb61733c6fb2abe757272247f9
My Name <cpgsom819@gmail.com>
2023-04-01 13:07:53 +0800

    adding appointment options

--- src/components/AvailableAppointment/AvailableAppointment.js
+++ src/components/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,33 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from '../AppointmentOption/AppointmentOption';
 
 const AvailableAppointment = ({selectedDate}) => {
+
+
+    const [appointmentOptions, setAppointmentOptions ] = useState([])
+
+
+
+    useEffect(() => {
+
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setAppointmentOptions(data))
+    })
+
     return (
         <div>
              <p className='text-center font-bold  text-primary'> AvailableAppoinment on   {format(selectedDate, 'PP')} </p>
+
+             <div>
+
+                {
+                    appointmentOptions.map(options => <AppointmentOption
+                    options={options}
+                    ></AppointmentOption>)
+                }
+             </div>
         </div>
     );
 };
