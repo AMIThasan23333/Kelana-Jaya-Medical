329746aee87d2365c394672cbb524906ffe55189
My Name <cpgsom819@gmail.com>
2023-04-01 12:21:19 +0800

    selectiong a date

--- src/components/AppointmentBanner/AppointBanner.js
+++ src/components/AppointmentBanner/AppointBanner.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
+import { useState } from 'react';
 
 
 const AppointBanner = () => {
+
+
+    const [selectedDate, setSelectedDate] = useState(new Date ())
+
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='' />
           <div>
   
-                 <DayPicker></DayPicker>
+                 <DayPicker
+                 
+                 mode='single'
+                 selected={selectedDate}
+                 onSelect={setSelectedDate}
+                 
+                 ></DayPicker>
           </div>
         </div>
       </div>
