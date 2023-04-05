641be5b0200411cf65165e153c3474f19f3f7921
My Name <cpgsom819@gmail.com>
2023-04-05 20:53:54 +0800

    adding accessToken

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -11,10 +11,17 @@ const MyAppointment = () => {
 
 
     const { data : bookings = []} = useQuery({
-        queryKey : ['bookings', user?.email],
 
+        queryKey : ['bookings', user?.email],
         queryFn : async () => {
-            const res = await fetch(url)
+            const res = await fetch(url, {
+
+                headers : {
+                    authorization : `bearer  ${localStorage.getItem('accessToken')}`
+
+                    
+                }
+            })
             const data = await res.json()
             return data;
 
